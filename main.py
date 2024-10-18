3e360ce8038572b50655abce3625d2b556dfc762
Valera Savko <SavkoV_81@STUDENTS.MGKE.LOC>
2024-10-18 15:25:07 +0300

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def chisla(numbers): # создание функции
-    return [x**2 for x in numbers] # квадраты чисел
 l_list = [1, 2, 3, 4, 5] # создание списка
-list = chisla(l_list) # возвращение нового списка
+list = [x**2 for x in l_list] # возвращение нового списка
 print(list)  # Вывод
